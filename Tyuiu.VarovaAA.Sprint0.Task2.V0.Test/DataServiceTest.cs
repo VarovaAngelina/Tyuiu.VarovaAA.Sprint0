f1b85e1c66a48919c8efdc3b96be212b79be3b6c
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-09-11 23:31:12 +0500

    Task2

---
+++ Tyuiu.VarovaAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VarovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.VarovaAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Ангелина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Ангелина", res);
+        }
+    }
+}
